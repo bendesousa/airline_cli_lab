df7adc51bbbd8134c9cc83ed980c74092cf23a8a
bendesousa <bdsousa069@gmail.com>
2022-08-30 16:07:28 +0100

    wrote passenger class

--- airline_cli_lab/src/main/java/Passenger.java
+++ airline_cli_lab/src/main/java/Passenger.java
@@ -1,2 +1,36 @@
 public class Passenger {
+
+    private String name;
+    private int phoneNumber;
+    private int id;
+
+    public Passenger (String name, int phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.id = (int) (100*Math.random());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = (int) (100*Math.random());
+    }
 }
