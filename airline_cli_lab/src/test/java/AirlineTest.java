891993d707d2dcaa55ee4deda9a63d89616d10ae
bendesousa <bdsousa069@gmail.com>
2022-08-31 09:41:01 +0100

    creaetd setup method for airline test

--- airline_cli_lab/src/test/java/AirlineTest.java
+++ airline_cli_lab/src/test/java/AirlineTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class AirlineTest {
+
+    private Airline airline;
+
+    @BeforeEach
+    public void setUp(){
+        this.airline = new Airline("BNA");
+    }
+    
+    @Test
+    public void canAddFlight(){
+
+    }
 }
