e0c3e9755fc5de9556f40358a7edeff1548c0867
bendesousa <bdsousa069@gmail.com>
2022-08-31 09:18:47 +0100

    began writing airline class

--- airline_cli_lab/src/main/java/Airline.java
+++ airline_cli_lab/src/main/java/Airline.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class Airline {
+
+    private String name;
+    private ArrayList<Flight> flightList;
+
+    public Airline (String name){
+        this.name = name;
+        this.flightList = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Flight> getFlightList() {
+        return flightList;
+    }
+
+    public void setFlightList(ArrayList<Flight> flightList) {
+        this.flightList = flightList;
+    }
+
+    public void addFlight (Flight flight){
+        this.flightList.add(flight);
+    }
+
+    public String cancelFlight (Flight flight){
+        this.flightList.remove(flight);
+        return "This flight has been cancelled.";
+    }
 }
